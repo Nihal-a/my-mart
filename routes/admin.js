630f7a8c0738bce5260c6f40b02279489f68bfa0
Nihal-a <nihalavulan1@gmail.com>
2020-11-25 11:07:03 +0530

    login and logout ready

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,13 @@ var router = express.Router();
 const adminHelpers=require('../helpers/admin-helper')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/dashboard');
+  let user=req.session.user
+  if(user){
+    res.render('admin/dashboard',{user});
+  }else{
+    res.redirect('/admin/login')
+  }
+  
 });
 router.get('/login',(req,res)=>{
   res.render('admin/login')
@@ -13,11 +19,17 @@ router.post('/login',(req,res)=>{
   adminHelpers.doLogin(req.body).then((response)=>{
     console.log("res---",response);
     if(response.status){
+      req.session.logedIn=true
+      req.session.user=response.user
       res.redirect('/admin')
     }else{
       res.redirect('/admin/login')
     }
   })
     
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/admin/login')
 })
 module.exports = router;
