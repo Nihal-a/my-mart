2c8ad8d1fea7d62cfa17a69c07c0e76ea806da71
Nihal-a <nihalavulan1@gmail.com>
2020-12-03 08:57:54 +0530

    dealer mennu ready

--- routes/dealer.js
+++ routes/dealer.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dealer/dealer-dashboard')
+  res.render('dealer/dealer-dashboard',{dealer:true})
 });
 
 module.exports = router;
